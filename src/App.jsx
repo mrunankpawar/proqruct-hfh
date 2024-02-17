5fad06cbe9618fc73239c4c2f42e351119efe15b
Mrunank Pawar <65391854+mrunankpawar@users.noreply.github.com>
2024-02-17 10:54:46 -0800

    initial files

---
+++ src/App.jsx
@@ -0,0 +1,143 @@
+import "./App.css";
+import { useState } from "react";
+
+const App = () => {
+  const [userName, setUserName] = useState("");
+  const [productName, setProductName] = useState("");
+  const [expiryDate, setExpiryDate] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // You can handle form submission logic here
+    console.log("Product Name:", productName);
+    console.log("Expiry Date:", expiryDate);
+  };
+
+  return (
+    <>
+      <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+        <div className="hidden sm:mb-8 sm:flex sm:justify-center">
+          <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+          Stay ahead of expiration dates with our handy notification system.{" "}
+          </div>
+        </div>
+        <div className="text-center">
+          <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+            Product Alert Assistance using ProQRuct
+          </h1>
+          <p className="mt-6 text-lg leading-8 text-gray-600">
+          Users can effortlessly stay ahead of expiring items, we are offering a solution for managing expiration dates by implementing a user-friendly notification system. 
+          </p>
+          <div className="mt-10 flex items-center justify-center gap-x-6">
+            <a
+              href="#"
+              className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Get started
+            </a>
+          </div>
+        </div>
+        <div
+          className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+          aria-hidden="true"
+        >
+          <div
+            className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(20%+36rem)] sm:w-[32.1875rem]"
+            style={{
+              clipPath:
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+            }}
+          />
+        </div>
+      </div>
+
+      <div className="max-w-md mx-auto mb-10 p-6 rounded-md shadow-lg shadow-blue-500/40">
+        <h2 className="text-2xl font-semibold mb-6">Enter product details</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label
+              htmlFor="userName"
+              className="block text-sm font-medium text-gray-600"
+            >
+              User Name
+            </label>
+            <input
+              type="text"
+              id="userName"
+              name="userName"
+              value={userName}
+              onChange={(e) => setUserName(e.target.value)}
+              className="shadow mt-1 p-2 w-full border rounded-md"
+              placeholder="John Doe"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-gray-600"
+            >
+              Email
+            </label>
+            <input
+              type="text"
+              id="email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="shadow mt-1 p-2 w-full border rounded-md"
+              placeholder="johndoe@gmail.com"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="userName"
+              className="block text-sm font-medium text-gray-600"
+            >
+              Product Name
+            </label>
+            <input
+              type="text"
+              id="userName"
+              name="userName"
+              value={productName}
+              onChange={(e) => setProductName(e.target.value)}
+              className="shadow mt-1 p-2 w-full border rounded-md"
+              placeholder="Milk"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="expiryDate"
+              className="block text-sm font-medium text-gray-600"
+            >
+              Expiry Date
+            </label>
+            <input
+              type="date"
+              id="expiryDate"
+              name="expiryDate"
+              value={expiryDate}
+              onChange={(e) => setExpiryDate(e.target.value)}
+              className="shadow mt-1 p-2 w-full border rounded-md"
+              required
+            />
+          </div>
+          <div className="mt-6">
+            <button
+              type="submit"
+              className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:shadow-outline-blue active:bg-blue-800"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default App;
